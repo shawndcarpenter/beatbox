c9f7efe400e0e7cd5cd422813a872bb3d5eeb283
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-08-24 18:33:16 -0600

    Set up Rspec for first half of Append, To String and Count

---
+++ spec/linked_list_spec.rb
@@ -0,0 +1,34 @@
+require "./lib/linked_list"
+require "./lib/node"
+
+RSpec.describe LinkedList do
+  it 'defines list.head as nil' do
+    list = LinkedList.new
+    expect(list.head).to be(nil)
+  end
+
+  it 'stores a value in @data' do
+    list = LinkedList.new
+    list.append("doop")
+    expect(list.head.data).to eq("doop")
+  end
+
+  it 'stores nil in next_node' do
+    list = LinkedList.new
+    list.append("doop")
+    expect(list.head.next_node).to be(nil)
+  end
+
+  it 'counts number of nodes' do
+    list = LinkedList.new
+    list.append("doop")
+    expect(list.count).to be(1)
+  end
+
+  it 'contains string of doop' do
+    list = LinkedList.new
+    list.append("doop")
+    expect(list.to_string).to eq("doop")
+  end
+
+end
